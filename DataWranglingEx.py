3c2199bafa529a5cbfec6f9cce9d94d5d45efa9a
Andriy Sinclair <andriysinclair@outlook.com>
2024-10-19 18:49:40 +0100

    Added a function to see if main earner is a female and then append all functions to the data frame - to merge with Andriy main branch

--- DataWranglingEx.py
+++ DataWranglingEx.py
@@ -45,4 +45,19 @@ def nr_female(x):
     count = len([i for i in x if i==True])
     return count
 
+def main_earner_female(x):
+    female_income = x[0]   # collects the first tuple in the list
+    for tup in x:  # loops through each tuple
+        if tup[0] == True and tup[1] > female_income[1]:  # if value is a female and income is higher than the first tuple
+            female_income = tup   # change the first tuple with that tuple
+    if female_income[0] == True:   # if at the end the tuple is a female - thus with higher incomee
+        return "yes"
+    else:
+        return "no"
+
+# applying functions to columns
+household_df["nr_children"] = household_df["nr_children"].apply(nr_children)
+household_df["nr_female"] = household_df["nr_female"].apply(nr_female)
+household_df["main_earner_female"] = household_df["main_earner_female"].apply(main_earner_female)
+
 print(household_df)
