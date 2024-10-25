828aef299e5766924854a394e7dd27ace5c54545
Andriy Sinclair <andriysinclair@outlook.com>
2024-10-25 16:50:41 +0100

    Made minor changes to all the modules. So that they can be imported into the notebook and used there to generate the variables. One change that woul be great is for the plots to display the actual country names rather than the codes

--- FED/load_data.py
+++ FED/load_data.py
@@ -6,23 +6,27 @@ import pandas as pd
 # print(wb.series.info(q="GDP")
 # The start year for the data
 # The end year for the data
+# Debug helps with debugging
 
-def load_data(countries, indicator, start_year, end_year):
+def load_data(countries, indicator, start_year, end_year, debug=False):
 
     # Loops through your list of countries and obtains the country code
     country_codes_list = []
     for country in countries:
         country_code = wb.economy.info(q=country).items[0]["id"] # Returns a table and the country code is the 0th entry
         country_codes_list.append(country_code)
-        print(country_code)
-        print("Printing country code for '{}' ...".format(country))
-        print("If you are thrown an error or you obtain an unexpected country code")
-        print("Please try a different query")
-        print("")
 
-    print("This is your list of country codes used to query the data")
-    print(country_codes_list)
-    print("")
+        if debug:
+            print(country_code)
+            print("Printing country code for '{}' ...".format(country))
+            print("If you are thrown an error or you obtain an unexpected country code")
+            print("Please try a different query")
+            print("")
+
+    if debug:
+        print("This is your list of country codes used to query the data")
+        print(country_codes_list)
+        print("")
 
     # Filters the WB API for the relevant data
     relevant_data = wb.data.DataFrame(indicator, country_codes_list, range(start_year, end_year))
@@ -32,10 +36,3 @@ def load_data(countries, indicator, start_year, end_year):
 
     # returns data frame
     return relevant_data
-
-
-data = load_data(countries=["UK", "United States", "Brazil", "Japan", "China", "Germany", "Switzerland"], indicator="NY.GDP.PCAP.PP.CD", start_year=1995, end_year=2024)
-
-print(type(data))
-
-#print(wb.economy.info(q="States"))
